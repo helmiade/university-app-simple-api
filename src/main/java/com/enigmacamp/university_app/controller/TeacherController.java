329f35a0b8bd7bb34f13ad2da1aeeea3bd42c7b9
Muhammad Helmi Ade Pamungkas <pamungkasade16@gmail.com>
2024-09-07 10:16:21 +0700

    finish create teacher controller class

---
+++ src/main/java/com/enigmacamp/university_app/controller/TeacherController.java
@@ -0,0 +1,41 @@
+package com.enigmacamp.university_app.controller;
+
+import com.enigmacamp.university_app.entity.Student;
+import com.enigmacamp.university_app.entity.Teacher;
+import com.enigmacamp.university_app.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class TeacherController {
+    private final TeacherService teacherService;
+
+    //    @GetMapping("/api/v1/students")
+    @RequestMapping("/api/v1/teachers")
+    public List<Teacher> getAllTeacher() {
+        return teacherService.getAllTeachers();
+    }
+
+    @GetMapping("/api/v1/teacher/{id}")
+    public Teacher getTeacherById(@PathVariable String id) {
+        return teacherService.getTeacherById(id);
+    }
+
+    @PutMapping("/api/v1/teacher")
+    public Teacher updateTeacher(@RequestBody Teacher teacher) {
+        return teacherService.updateTeacher(teacher);
+    }
+
+    @PostMapping("/api/v1/teacher")
+    public Teacher createTeacher(@RequestBody Teacher teacher) {
+        return teacherService.createTeacher(teacher);
+    }
+
+    @DeleteMapping("/api/v1/teacher/{id}")
+    public void deleteTeacher(@PathVariable String id) {
+        teacherService.deleteTeacher(id);
+    }
+}
